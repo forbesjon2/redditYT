a3821a7691a5c268b8ac1a1e6b9ed8435b8f0f1b
root <root@ip-172-31-22-218.ec2.internal>
2020-01-18 21:24:06 +0000

    added reddit scraping functionality, created html templates

--- ScrapeReddit.py
+++ ScrapeReddit.py
@@ -2,5 +2,50 @@
 
 
 class ScrapeReddit:
-    def __init__(self, board="news", verbose=True):
+    def __init__(self, redditInstance, numberOfTopPosts=10, board="news", timeFrame="day", verbose=True):
+        self.board = board
+        self.verbose = verbose
+        self.timeFrame = timeFrame
+        self.redditInstance = redditInstance
+        self.numberOfTopPosts = numberOfTopPosts
+
+    def returnTop(self):
+        """
+        prints and Returns a list of the top submissions
         
+        returns a list of submissions objects see the following for info:
+        https://praw.readthedocs.io/en/latest/code_overview/models/submission.html
+        """
+        count = 0
+        submissions = []
+        for submission in self.redditInstance.subreddit(self.board).top(self.timeFrame):
+            if count > self.numberOfTopPosts:
+                break
+            print(str(count) + ". " +  submission.title + "\n")
+            submissions.append(submission)
+            count += 1
+        return submissions
+
+
+
+    def getTopComments(self, numTopComments, post):
+        """
+        this will return the top comments and the reply attached
+        to each of the top comments
+
+        given submissionsList from returnTop()...
+        post = submissionsList[0].comments.list()
+
+        for comments and replies you will need to use....
+        comment.author, comment.body, comment.score
+        (https://praw.readthedocs.io/en/latest/code_overview/models/comment.html)
+
+        format
+        [[comment, reply], [comment, reply]]
+        """
+        res = []
+        count = 0
+        while count < numTopComments:
+            res.append([post[count], post[count].replies.list()[0]])
+            count += 1
+        return res
--- sandbox.py
+++ sandbox.py
@@ -1,7 +1,16 @@
 import praw
+from ScrapeReddit import ScrapeReddit
+
 reddit = praw.Reddit(client_id='T_2TohvDxVxynw', client_secret="yQzjuCPfBijseEYLnrezj05ELKQ",
                      password='moKkun-dotrif-sezxo0', user_agent='Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.4 Safari/605.1.15',
                      username='markermal3')
+                
+sr = ScrapeReddit(reddit, 10, "news", "day", True)
+submissionsList = sr.returnTop()
 
-for submission in reddit.subreddit('news').hot(limit=25):
-    print(submission.title)
+post = submissionsList[0].comments.list()
+topComments = sr.getTopComments(10, post)
+for comment in topComments:
+    print(comment[0].body)
+    print(comment[1].body)
+    print ("\n")
